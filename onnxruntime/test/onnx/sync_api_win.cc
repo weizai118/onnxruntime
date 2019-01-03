b7d32212a8755d2d3362b094f5772b682a4d5771
Ashwin Kumar <ashku@microsoft.com>
2019-01-02 21:43:38 -0800

    use waitforworkcomplete

--- onnxruntime/test/onnx/sync_api_win.cc
+++ onnxruntime/test/onnx/sync_api_win.cc
@@ -9,7 +9,6 @@ using ::onnxruntime::common::Status;
 
 static std::unique_ptr<onnxruntime::TaskThreadPool> default_pool;
 static std::once_flag default_pool_init;
-static std::unique_ptr<std::vector<std::future<void>>> task_results;
 
 PThreadPool GetDefaultThreadPool(const onnxruntime::Env& env) {
   std::call_once(default_pool_init, [&env] {
@@ -24,27 +23,18 @@ Status CreateAndSubmitThreadpoolWork(ORT_CALLBACK_FUNCTION callback, void* data,
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "must provide a threadpool to run the tests concurrently on");
   }
   std::packaged_task<void()> work{std::bind(callback, nullptr, data)};
-  task_results->push_back(work.get_future());
   pool->RunTask(std::move(work));
   return Status::OK();
 }
 
 Status WaitAndCloseEvent(ORT_EVENT finish_event) {
-  DWORD dwWaitResult = WaitForSingleObject(finish_event, INFINITE);
+  /* DWORD dwWaitResult = WaitForSingleObject(finish_event, INFINITE);
   (void)CloseHandle(finish_event);
   if (dwWaitResult != WAIT_OBJECT_0) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "WaitForSingleObject failed");
-  }
+  }*/
 
-  try {
-    // wait for all and propagate any exceptions
-    for (auto& future : *task_results)
-      future.get();
-  } catch (const std::exception&) {
-    throw;
-  }
-
-  task_results->clear();
+  default_pool->WaitWorkComplete();
   return Status::OK();
 }
 
@@ -60,23 +50,12 @@ Status CreateOnnxRuntimeEvent(ORT_EVENT* out) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "unable to create finish event");
   }
   *out = finish_event;
-  task_results = std::make_unique<std::vector<std::future<void>>>();
   return Status::OK();
 }
 
 Status OnnxRuntimeSetEventWhenCallbackReturns(ORT_CALLBACK_INSTANCE, ORT_EVENT finish_event) {
   if (finish_event == nullptr)
     return Status(::onnxruntime::common::ONNXRUNTIME, ::onnxruntime::common::INVALID_ARGUMENT, "");
-
-  ////try {
-  ////  // wait for all and propagate any exceptions
-  ////  for (auto& future : task_results)
-  ////    future.get();
-  ////} catch (const std::exception&) {
-  ////  throw;
-  ////}
-
-  ////task_results.clear();
 
   if (!SetEvent(finish_event)) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "SetEvent failed");
